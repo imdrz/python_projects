3e754e33eacd75002b033ab6d068fd3a63cc5a9b
imdrz <imadriaz09@gmail.com>
2021-04-11 14:21:32 -0400

    Update README, add some info to triangular_numbers.py

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ My random python programs
 ### List of programs
 
 #### Triangular number generator
-Generates a list of triangular numbers.
+Generates a list of triangular numbers. Triangular numbers are any series of numbers by continued summation, or adding, of numbers. Example: 1, 3, 6, 10. The formula to find the triangular numbers is (n*(n+1))/2, where n is the last number. You would continuesly do that for how many triangular numbers you want. 😉
--- triangular_numbers.py
+++ triangular_numbers.py
@@ -9,5 +9,14 @@ def calc():
     n+=1
   print(numbers)
 
+  yesorno = input("Do You Want To Learn What A Triangular Number Is? [Y/N] ")
+
+  if yesorno.lower() == "y":
+    print()
+    print("Triangular Numbers:")
+    print("Generates a list of triangular numbers. Triangular numbers are any series of numbers by continued summation, or adding, of numbers. Example: 1, 3, 6, 10. The formula to find the triangular numbers is (n*(n+1))/2, where n is the last number. You would continuesly do that for how many triangular numbers you want. 😉 ")
+  else:
+    print()
+
 
 calc()
